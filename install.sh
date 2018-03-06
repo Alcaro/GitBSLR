ca1112cc85e9039f15a65dd1474f322bcf0f54d8
Alcaro <floating@muncher.se>
2018-03-06 18:59:19 +0100

    Add install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+make OPT=1 -j8 || exit $?
+GITBSLR=$(readlink -f $(dirname $0))/gitbslr.so
+#TODO: figure out how much escaping needs to be done to make this append to LD_PRELOAD, rather than overwrite it
+NEWLINE='alias git="LD_PRELOAD='$GITBSLR' git"'
+grep -q -F $GITBSLR ~/.bashrc || echo $NEWLINE >> ~/.bashrc
+echo "Installed for user $USER under Bash, reload your ~/.bashrc"
