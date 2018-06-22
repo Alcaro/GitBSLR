bd335b6be86789a49bb23bdf4b171dec3863dda0
Alcaro <floating@muncher.se>
2018-06-22 15:06:22 +0200

    Installing a wrapper script under ~/bin/ seems saner, and more portable, than adding an alias

--- install.sh
+++ install.sh
@@ -4,9 +4,13 @@
 
 make OPT=1 || exit $?
 GITBSLR=$(readlink -f $(dirname $0))/gitbslr.so
-#TODO: figure out how much escaping needs to be done to make this append to LD_PRELOAD, rather than overwrite it
-#TODO: figure out if LD_PRELOAD even supports preloading multiple things
-#TODO: if doing this, fix the unsetenv call too
-NEWLINE='alias git="LD_PRELOAD='$GITBSLR' git"'
-grep -q -F $GITBSLR ~/.bashrc || echo $NEWLINE >> ~/.bashrc
-echo "Installed for user $USER under Bash, restart your shell or reload your ~/.bashrc"
+[ -d ~/bin/ ] || mkdir ~/bin/
+[ -e ~/bin/git ] && rm ~/bin/git
+GITORIG=$(which git)
+cat > ~/bin/git << EOF
+#!/bin/sh
+export LD_PRELOAD=$GITBSLR
+exec $GITORIG "\$@"
+EOF
+chmod +x ~/bin/git
+echo "Installed for user $USER"
