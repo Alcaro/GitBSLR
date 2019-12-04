90e2b878d44794a3decccfc49d875d5ebc2d4ffc
Alcaro <floating@muncher.se>
2019-12-04 12:15:20 +0100

    Improve install.sh output, fixes #10

--- install.sh
+++ install.sh
@@ -10,7 +10,12 @@ set -eu
 TARGET="$HOME/bin"
 [ $(id -u) -eq 0 ] && TARGET="/usr/local/bin"
 
-[ -d $TARGET ] || mkdir $TARGET
+CREATED_TARGET=0
+
+if [ ! -d $TARGET ]; then
+  mkdir TARGET
+  CREATED_TARGET=1
+fi
 if [ -e $TARGET/git ]; then
   if grep -q gitbslr.so $TARGET/git; then
     rm $TARGET/git
@@ -45,7 +50,32 @@ chmod +x $TARGET/git
 chmod -x $TARGET/gitbslr.so
 
 if [ "$(which git)" != $TARGET/git ]; then
-  echo "warning: installed to $TARGET/git, but $TARGET/ is not in your PATH (or another Git is in front of $TARGET/); fix that to complete the installation"
-elif [ "$(which git)" = /usr/local/git ]; then
+  case ":$PATH:" in
+  *:$TARGET:*)
+    echo "warning: installed to $TARGET/git, but another Git is already in your \$PATH, in front of $TARGET/; fix that to complete the installation"
+    ;;
+  *:$TARGET/:*)
+    # can't find a way to deduplicate these branches
+    echo "warning: installed to $TARGET/git, but another Git is already in your \$PATH, in front of $TARGET/; fix that to complete the installation"
+    ;;
+  *)
+    # don't source .profile, it's incompatible with set -u
+    if [ $CREATED_TARGET = 1 ] && [ $(sh -c ". ~/.profile; which git") = $TARGET/git ]; then
+      echo "Installed GitBSLR to $TARGET/git; run"
+      echo "  export PATH=\"\$HOME/bin:\$PATH\""
+      echo "or restart your terminal to complete the installation"
+    elif [ TARGET = "$HOME/bin" ]; then
+        echo "Installed GitBSLR to $TARGET/git; run"
+        echo "  echo 'export PATH=\"\$HOME/bin:\$PATH\"' >> ~/.profile"
+        echo "and restart your terminal to complete the installation"
+    else
+      echo "warning: installed GitBSLR to $TARGET/git, but $TARGET/ is not in your PATH; fix that to complete the installation"
+    fi
+    ;;
+  esac
+else
   echo "Installed GitBSLR to $TARGET/git"
 fi
+echo "To verify if GitBSLR is correctly installed, run"
+echo "  GITBSLR_DEBUG=1 git version"
+echo "and check if it says \"GitBSLR: Loaded\"."
